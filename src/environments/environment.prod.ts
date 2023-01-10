f6ec1555411399b9d80d4ea1fe52c2ea3dc708d6
carlos caicedo <ecm4717e@claro.com.co>
2023-01-10 09:23:59 -0500

    aaaa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export let environment = {
   production: true,
   env: 'production',
-  url_biblioteca: 'http://localhost:8080/'
+  url_biblioteca: 'https://backend-openshift-git-lealjc-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export let environment = {
   production: false,
   env: 'production',
-  url_biblioteca: 'http://localhost:8080/'
+  url_biblioteca: 'https://backend-openshift-git-lealjc-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/'
 };
 
